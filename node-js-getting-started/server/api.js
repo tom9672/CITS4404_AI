991763449144241e54132210ddf302d017c33104
tomzhou <18896825080@163.com>
2020-09-24 20:07:05 +0800

    Add cool face API

---
+++ node-js-getting-started/server/api.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const { json } = require('express');
+
+const app = express();
+
+app.use('/', express.static(path.join(__dirname, '../public')));
+
+
+
+app.listen(7000, function () {
+    console.log('started');
+    
+});
